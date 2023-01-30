2b47752b4bddc6c2f1dce90c4e99e93793e2292f
Chelsie Ty <chelsiety@gmail.com>
2023-01-30 16:47:58 +0800

    Create function that repeats strings
    - create for loop
    - test with JEST (pass first test)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,32 @@
-const repeatString = function() {
+// Function expression
+
+
+const repeatString = function(aString, stringMultiplier) {
+    let appendedString = ""
+    for(let i = 0; i < stringMultiplier; i++){
+        appendedString += aString
+    }
+    return appendedString
+
+
+
+
+
+
+
+
+
+
 
 };
 
+/* 
+- Use a loop 
+- repeats the given number of times and add given string to the result on each loop
+- create a variable to hold string that you're going to return
+
+
+
+*/
 // Do not edit below this line
 module.exports = repeatString;
