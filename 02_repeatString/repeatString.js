9e87d122e32c2c5d206745db0384b4cdbccbd11d
Chelsie Ty <chelsiety@gmail.com>
2023-01-30 17:15:15 +0800

    Add conditions to repeatString function
    - Add if statement for repeating the string 0 times
    - Add if statement to return ERROR with negative numbers as input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,30 +3,23 @@
 
 const repeatString = function(aString, stringMultiplier) {
     let appendedString = ""
+
+    if (stringMultiplier === 0){
+        return ''
+    }
+    else if (stringMultiplier < 0){
+        return 'ERROR'
+    }
+    else if (stringMultiplier > 0){
+
+    
     for(let i = 0; i < stringMultiplier; i++){
         appendedString += aString
     }
     return appendedString
-
-
-
-
-
-
-
-
-
-
+    }
 
 };
 
-/* 
-- Use a loop 
-- repeats the given number of times and add given string to the result on each loop
-- create a variable to hold string that you're going to return
-
-
-
-*/
 // Do not edit below this line
 module.exports = repeatString;
