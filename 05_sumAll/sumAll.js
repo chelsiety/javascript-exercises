716ec12102c61bf372c30318c7073db8dfcf7830
Chelsie Ty <chelsiety@gmail.com>
2023-02-03 02:16:09 +0800

    Create a sumAll function - adds all numbers within the given range - works with large numbers - works with larger number first - returns ERROR with negative numbers - returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'ERROR';
+    }
+    else if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    else {
+        const startNum = Math.min(num1, num2);
+        const endNum = Math.max(num1, num2);
+
+        let sum = 0;
+        for (let i = startNum; i <= endNum; i++){
+            sum += i;
+        }
+        return sum; 
+    }
 };
 
 // Do not edit below this line
